9e0c9b30e037c5fd37fb109ec9f8d631ae93adab
lillian <lillian.m.zhao@gmail.com>
2024-06-21 19:14:02 -0400

    Update project-filter.js

--- docs/assets/js/project-filter.js
+++ docs/assets/js/project-filter.js
@@ -143,7 +143,7 @@ $(document).ready(function () {
         });
         console.log("This is the logging of year")
         console.log(year)
-        console.log("This is the logging of object keys")
+        console.log("This is the logging of reverse object keys")
 
         yearsList = (Object.keys(projectsByYear)).reverse()
 
